fc407341c800c7925f0f1818779b7c8fbb84698d
ooiiww <ooiiww@gmail.com>
2012-02-23 21:06:17 +0800

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Teamwork::Application.config.secret_token = 'c53bbc371d32f5ccdf66023121a00736480923f8dbf35e61b88a18bbc83ec281c3c8b232501b875a9192108ccbc466c67b2ccfcb2dab2396c91a45e6b0ca3f27'
